e31d3c9a204d0934d71ad6bde8e49290a73e764b
basma123456 <72777076+basma123456@users.noreply.github.com>
2020-10-22 12:41:50 -0700

    Update app.js

--- app.js
+++ app.js
@@ -23,11 +23,12 @@ const navContainer = document.getElementById('myNav');
 const navUl = document.createElement('ul');
 //////////////////////////////////////////////////////
 
-
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 /**
  * ##first function related to the nav part
+ 
  * @description make function to loop over the sections 
- * create nav items which are <li> and <a> tags and appending them to the ul tag
+ * @return create nav items which are <li> and <a> tags and appending them to the ul tag
  * extracting data-nav attributes values from sections and give each of them to the textContent of the each of links in nav part
  * set data-section attribute for <a> and give to each value of the refrenced sections's id and that for the usage after that in the scrollIntoview Method
  * adding styles to anv items
@@ -79,7 +80,7 @@ mySections.forEach(mySection => {
 navContainer.appendChild(navUl);
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //##sections part
 
@@ -88,7 +89,7 @@ navContainer.appendChild(navUl);
  * @description declare options variable which is will used after that in the IntersectionObserver constructor
  * @returns
  * consists of root : null   mean that web server api will be the root
- * threshold : 0.3   mean that if 0.3 of the section onle will be shown in the screen it will gain the active state
+ * threshold : 0.3   mean that if 0.3 of the section onle will be shown in the screen it will gain the active status
  * rootMargin : "-150px 0px 0px 0px"  it considered to be an ordinary margin but to the the root 
  */
 
@@ -98,31 +99,43 @@ const options = {
     threshold : 0.3,
     rootMargin : "-150px 0px 0px 0px"
 };
+/////////////////////////////////////////////////////
 
 /**
  * ##make IntersectionObserver constructor
  * @description observing element
- * @param {function} it needs two parameters : the first is internal function , 
- * @param {options} the second is options
+ * @param {call back function} it needs two parameters : the first is internal function , 
+ 
  * first parameter :
-         **internal function : 
-         * @description its inside the constructor as a parameter to it and it needs two paramenters
-         * @param {entries}
-         * @param {observer}
-         ** first paramenetr will be (entries) which represent the sections 
-         ** second parameter which is the the same constuctor it self
-         ** loop for each entry which maen loop for each section
-         ** make if condition that state in the case of the section not intersect the visible screen we will remove the active class from it
-         ** and in case of opposite to that condition or in the intersection or the active state it wiil gain the active class
-         ** loop over each <a> in the nav and get the textContent of each <a>
-         ** get the data-nav attribute value of the intersecting visible section on the screen
-         ** if the data-nav equal the textContet of each anchor then it is the active case then gain the anchor green background
-         ** else give it navy background
- * second parameter : 
- * is options which is declared before the function
- *  *[refrence to IntersectionObserver constructor](https://www.youtube.com/watch?v=RxnV9Xcw914)
+                ////////////////////////////////////////////////////////////////////////////////////////////////
+                 **call back function : 
+                 
+                 ** @description its inside the constructor as a parameter to it which is A function 
+                 which is called when the percentage of the target element is visible crosses a threshold. The callback receives as input two parameters
+                 
+                 ** @param {entries} represent the sections which is An array of IntersectionObserverEntry objects, 
+                 each representing one threshold which was crossed, either becoming more or less visible than the percentage specified by that threshold.
+                 
+                 ** @param {observer} the name of hte constructor which is The IntersectionObserver for which the callback is being invoked.
+
+                 ** @return loop for each entry which maen loop for each section
+                 ** make if condition that state in the case of the section not intersect the visible screen we will remove the active class from it
+                 ** and in case of opposite to that condition or in the intersection or the active state it wiil gain the active class
+                 ** loop over each <a> in the nav and get the textContent of each <a>
+                 ** get the data-nav attribute value of the intersecting visible section on the screen
+                 ** if the data-nav equal the textContet of each anchor then it is the active case then gain the anchor green background
+                 ** else give it navy background
+                 //////////////////////////////////////////////////////////////////////////////////////////////////
+                 
+ * @param {options} the second is options represent properties of the view port or the visible part of the entry which is now reprsented in the screen 
+ 
+ * @return  A new IntersectionObserver which can be used to watch for the visibility of a target element within the specified root 
+ crossing through any of the specified visibility thresholds. Call its observe() method to begin watching for the visibility changes on a given target.
+ 
+ *  [refrence to IntersectionObserver constructor](https://www.youtube.com/watch?v=RxnV9Xcw914)
  */
 
+///////////////////////////////////////////////////////////////////////////////////
 
 const observer = new IntersectionObserver(function(entries,observer){
 
@@ -170,7 +183,7 @@ const observer = new IntersectionObserver(function(entries,observer){
 
 }, options);
 
-
+////////////////////////////////////////////////////////////////////
 
 /**
  * @description loop for each section to apply the IntersectionObserver constructor to each section
@@ -184,22 +197,34 @@ const observer = new IntersectionObserver(function(entries,observer){
 });
 
 
-////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 /**
  * 
  * ##scroll part
- * 
  * get the data-section attribute of each anchor 
  * because of each of sections has id equals to the data-nav attribute of the refrenced anchor to it
  * inquire about the element thet has id eqauls to the data-section of clicked anchor in the case of clicking the anchor by addEventListenet method
  * in the case of clicking the anchor it will scrolled to the section that have that id
  * make the scroll be smooth an locate the section in the center by apply scrollItoView method to the chosen section
-         * ##scrollItoView method :
+ 
+         ** ##scrollItoView method :
+         ** @descreption push element to certain destination by smooth scroll or
+         The Element interface's scrollIntoView() method scrolls the element's parent container
+         such that the element on which scrollIntoView() is called is visible to the user
+         
          ** has two parameters :
-         **the first is about the behavior of the transfer between each two items (smooth)
-         **the second is about the way of represented the selected item as in the first of the screen or in the center or in the end
+         ** @param {behavior} behavoir of scroll or 
+            Defines the transition animation.
+            One of auto or smooth. Defaults to auto, but we choose smooth
+         ** @param {block} 
+         Defines vertical alignment or
+         the way of showing the targeted element in a certain destination we choose here  center
+         
+         
+         **@return smooth scroll to a certain targeted element in a certain destination
+         
  *[refrence to scroll function](https://www.youtube.com/watch?v=23JQzsz9BlY)
  */
 
@@ -251,9 +276,15 @@ const optionsTwo = {
 /**
  * constructor observorTwo
  * @description InterSectionObserver constructor make loop to each entry 
- * and make if conditional case of not intersecting 
- * @param {entries}
- * @param {options variable}  // represented properties of the entry
+  and make if conditional case of not intersecting 
+  
+ * @param {entries} An array of IntersectionObserverEntry objects, 
+ each representing one threshold which was crossed, either becoming more or less visible than the percentage specified by that threshold.
+ 
+ * @param {options}  // represented properties of the entry
+ or An optional object which customizes the observer. If options isn't specified, the observer uses the document's viewport as the root,
+ with no margin, and a 0% threshold (meaning that even a one-pixel change is enough to trigger a callback)
+ 
  * @returns in case of not intersecting of the entry to the view port 
  * the display method of the (top button) will be block else it will gain  none  
  * 
